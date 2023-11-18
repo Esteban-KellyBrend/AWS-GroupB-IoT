208225a8583f6a35d53e76c95e1f1d2b06f19d11
Aloysius Butac <aloysius.atheos.butac@adamson.edu.ph>
2023-11-18 11:26:16 +0800

    Squashed commit of the following:
    commit 4e59499e076c0bd9620aca288fdebb82bd98224d
    Merge: 9733aaa c2385b3
    Author: Poncholo Riego de Dios <89110768+Poncholo-Riego-de-Dios@users.noreply.github.com>
    Date:   Sat Nov 18 00:13:32 2023 +0800
    
        Merge pull request #6 from Esteban-KellyBrend/Hakdog
    
        Hakdog - Integration of Log In Page
    
    commit c2385b3e3e6c01bbf52224fecb0f4d3398eae50f
    Author: Poncholo Riego de Dios <poncholo.riego.de.dios@adamson.edu.ph>
    Date:   Sat Nov 18 00:09:33 2023 +0800
    
        Integration of Log In Page
    
    commit 9733aaaedb196e49d5dc01edc1a2d5c6d1ba126c
    Merge: 34fcc85 d767c78
    Author: Vince Villasor <78065232+vincekzir@users.noreply.github.com>
    Date:   Fri Nov 17 21:06:40 2023 +0800
    
        Merge pull request #5 from Esteban-KellyBrend/Bins
    
        Added Display for Dashboard
    
    commit d767c785db032aed069eed03953bd740180162b2
    Author: Vince Villasor <vince.kazer.villasor@adamson.edu.ph>
    Date:   Fri Nov 17 21:05:48 2023 +0800
    
        Added Display for Dashboard
    
    commit 7b9aec9ce0c383fff9f0cef0c42b46c34dde849b
    Merge: c459238 639ffaa
    Author: Poncholo Riego de Dios <poncholo.riego.de.dios@adamson.edu.ph>
    Date:   Thu Nov 16 14:08:13 2023 +0800
    
        Merge branch 'Kel' into Hakdog
    
    commit c4592388ba924e5693837fb62b28bdefb5c92f09
    Author: Poncholo Riego de Dios <poncholo.riego.de.dios@adamson.edu.ph>
    Date:   Thu Nov 16 14:06:29 2023 +0800
    
        Adjustments
    
    commit 639ffaa2d7e2e06ba42ad9eef198a909f20fc029
    Author: Kel24 <78207376+Kel24@users.noreply.github.com>
    Date:   Thu Nov 16 00:09:54 2023 +0800
    
        Added Log Page

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,37 @@
 import React from "react";
+import "./Home.css";
 import { Link } from "react-router-dom";
 
-function Home() {
-  return (
-    <div className="bg-gradient-to-tr to-[#431857] from-black from-30% bg-cover absolute h-screen w-screen">
-      <div className="flex justify-center items-center mt-[40vh]">
-        <Link to="/dashboard" className="text-white bg-purple-600 text-3xl px-3 py-1 rounded-md border-white border-2">
-          Dashboard
-        </Link>
-      </div>
-    </div>
-  );
+const Home = () => {
+    return (
+        <div className="login flex-col max-w-2xl mt-30 ml-6">
+          <div className="mt-24">
+            <div className="title">
+              <h1 className="font-bold text-4xl">Automatic Weather Station</h1>
+            </div>
+            <div className="locationtitle">
+              <h4 className="font-bold text-1.5xl mt-3">Caloocan</h4>
+            </div>
+
+            <div className="input-box">
+                <input type="text" placeholder="Username"/>
+            </div>
+
+            <div className="input-box">
+                <input type="text" placeholder="Email"/>
+            </div>
+
+            <div className="input-box">
+                <input type="password" placeholder="Password"/>
+            </div>
+
+            <div>
+                {/* Use to="/dashboard" instead of to={/dashboard} */}
+                <Link to="/dashboard" className="btn">LOG IN</Link>
+            </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;

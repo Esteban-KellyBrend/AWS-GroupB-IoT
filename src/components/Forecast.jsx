b5def1752a57609a4a0fd1429e9975a591b01974
Poncholo Riego de Dios <poncholo.riego.de.dios@adamson.edu.ph>
2023-11-20 20:21:28 +0800

    Adjustments, Organization, and New Components
    -Fixed the positioning of all components in dashboard page
    -Created new Forecast Component
    -Created new Temperature component
    -Created new Time component
    -Adjusted the sizing of texts and containers
    -Converted all js to jsx files

---
+++ src/components/Forecast.jsx
@@ -0,0 +1,71 @@
+import { Icon } from "@iconify/react";
+import React from "react";
+
+function Forecast() {
+  const WForecast = [
+    {
+      iconn: "material-symbols:sunny-outline",
+      Weather: "SUNNY",
+      Time: "7:00",
+    },
+    {
+      iconn: "material-symbols:sunny-outline",
+      Weather: "SUNNY",
+      Time: "7:30",
+    },
+    {
+      iconn: "fontisto:day-cloudy",
+      Weather: "PARTLY CLOUDY",
+      Time: "8:00",
+    },
+    {
+      iconn: "fluent-mdl2:cloudy",
+      Weather: "CLOUDY",
+      Time: "8:30",
+    },
+    {
+      iconn: "fluent-mdl2:cloudy",
+      Weather: "CLOUDY",
+      Time: "9:00",
+    },
+    {
+      iconn: "mingcute:cloud-windy-line",
+      Weather: "WINDY",
+      Time: "9:30",
+    },
+    {
+      iconn: "carbon:rain",
+      Weather: "RAINY",
+      Time: "10:00",
+    },
+    {
+      iconn: "raphael:thunder",
+      Weather: "STORMY",
+      Time: "10:30",
+    },
+  ];
+
+  return (
+    <div className="mt-2 mb-4">
+      <p className="text-white flex text-xl text-opacity-70 pb-1 indent-3">
+        Weather Condition
+      </p>
+      <div className="w-full h-[22vh] rounded-2xl border border-white border-opacity-30 bg-[#4D4D4D] bg-opacity-10 flex flex-row justify-center items-center">
+        <section className="flex justify-around gap-1 p-1">
+          {WForecast.map((Weath, index) => (
+            <div key={index} className="w-[10vw] h-full rounded-xl flex flex-col justify-center items-center p-1">
+              <Icon
+                icon={Weath.iconn}
+                className="border-none text-white text-[4vw] bg-transparent mb-3"
+              />
+              <p className="text-sm text-white text-opacity-80">{Weath.Weather}</p>
+              <p className="text-xs text-white text-opacity-60 -mt-1">{Weath.Time}</p>
+            </div>
+          ))}
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default Forecast;
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,12 +1,12 @@
 const Heading = ({ type, name }) => {
     return (
         (type === 1) ? (
-            <div className="flex-grow m-4">
-                <p className="text-[48px] text-[#C8A6DF] font-bold">
+            <div className="flex-grow">
+                <p className="text-[38px] text-[#C8A6DF] font-bold">
                     AUTOMATIC WEATHER SYSTEM (NORTH)
                 </p>
-                <hr className=" border-[#BFBFBF] w-1/2" />
-                <p className="text-[#DCC0EA] text-xl text-opacity-70 -mt-1">{name}</p>
+                <hr className=" border-[#BFBFBF] w-1/3" />
+                <p className="text-[#DCC0EA] text-lg text-opacity-70 -mt-1">{name}</p>
             </div>
         ) : null  // Add this line to handle the case when type is not equal to 1
     );

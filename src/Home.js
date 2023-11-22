ae25a83d02d188ee85efebfed9fef0b4a3ca0c6e
Vince Villasor <vince.kazer.villasor@adamson.edu.ph>
2023-11-23 02:00:29 +0800

    Fixed Login and Signup Button

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ import LoginForm from "./components/LoginForm";
 const Home = () => {
     return (
         <div className="bg-gradient-to-tr to-[#431857] from-black from-30% bg-cover absolute h-screen w-screen" >
-            <div className="login flex-col max-w-2xl mt-20 ml-9 flex-grow items-center justify-start text-[#C5A4D5] w-1/2">
+            <div className="login flex-col max-w-2xl mt-20 ml-16 flex-grow items-center justify-start text-[#C5A4D5] w-1/2">
                 <div className="title">
                     <h1 className="font-bold text-5xl">Automatic Weather Station</h1>
                 </div>

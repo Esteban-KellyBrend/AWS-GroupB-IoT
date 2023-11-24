9ef7f3587c07485316381c895a55562e2a56b426
Vince Villasor <vince.kazer.villasor@adamson.edu.ph>
2023-11-24 16:21:07 +0800

    NavBar Fix

--- src/pages/statistics/WindSpeedStats.jsx
+++ src/pages/statistics/WindSpeedStats.jsx
@@ -1,8 +1,10 @@
 import LineGraph from "../../components/LineGraph";
+import Navbar from "../../components/Navbar";
 
 function WindSpeedStats() {
   return (
     <div className=" bg-gradient-to-tr to-[#431857] from-black from-30% bg-cover absolute h-screen w-screen">
+    <Navbar />
     <LineGraph />
     </div>
   );

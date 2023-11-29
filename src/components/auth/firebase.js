29d12d8ede732b1988445da5eccceabb78bcb590
CustodioJustin <78063106+CustodioJustin@users.noreply.github.com>
2023-11-29 20:52:52 +0800

    include database

--- src/components/auth/firebase.js
+++ src/components/auth/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration, replace it with your project keys
 const firebaseConfig = {
@@ -9,11 +10,13 @@ const firebaseConfig = {
   projectId: "iot-project-aws",
   storageBucket: "iot-project-aws.appspot.com",
   messagingSenderId: "356573332647",
-  appId: "1:356573332647:web:a6473138b53cc6baf68782"
+  appId: "1:356573332647:web:a6473138b53cc6baf68782",
+  databaseURL: "https://iot-project-aws-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);

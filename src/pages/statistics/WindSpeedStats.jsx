33f95e1fe3c258d1f043148323ef0317f70c4a08
CustodioJustin <78063106+CustodioJustin@users.noreply.github.com>
2023-12-01 20:47:15 +0800

    now has realtime update on data changes
    fix stupidity.

--- src/pages/statistics/WindSpeedStats.jsx
+++ src/pages/statistics/WindSpeedStats.jsx
@@ -2,7 +2,8 @@ import LineGraph from "../../components/LineGraph";
 import Navbar from "../../components/Navbar";
 import Heading from "../../components/Heading";
 import Display from "../../components/Display";
-import * as FirebaseHandler from '../../components/database/FirebaseHandler';
+import { useAllDataFromFirebase, useLimitedDataFromFirebase } from "../../components/database/FirebaseHandler";
+import { formatDataKeys, GetLowHighAveData } from "../../components/database/DataDisplayHandler";
 
  /*
    *
@@ -14,17 +15,19 @@ import * as FirebaseHandler from '../../components/database/FirebaseHandler';
    */
 
 
+const hourlyValues = [10, 9, 7, 13, 1, 8, 16];
 const weeklyData = [10, 9, 7, 13, 1, 8, 16];
 const weeklyLabels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const hourlyLabels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 function WindSpeedStats() {
 
-  const LowestSpeed = FirebaseHandler.GetDataStatistics('averageWindSpeed','lowest')
-  const AverageSpeed = FirebaseHandler.GetDataStatistics('averageWindSpeed','average')
-  const HighestSpeed = FirebaseHandler.GetDataStatistics('averageWindSpeed','highest')
-  const hourlyData = FirebaseHandler.GetRealTimeDatas('averageWindSpeed');
-  const hourlyLabels = hourlyData.map(entry => entry.key);
-  const hourlyValues = hourlyData.map(entry => entry.data);  
+  const data = useLimitedDataFromFirebase("averageWindSpeed", 17);
+  const datakey = data.map(entry => entry.key);
+  const datavalue  = data.map(entry => entry.value);
+  const filtereddatakey = formatDataKeys(datakey);
+
+  const [averageValue, lowestValue, highestValue] = GetLowHighAveData(datavalue)
 
   return (
     <div className="bg-gradient-to-tr to-[#431857] from-black from-30% bg-cover absolute h-screen w-screen">
@@ -33,15 +36,15 @@ function WindSpeedStats() {
         <Heading type={1} name={"WIND SPEED"} />
       </section>
 
-      <LineGraph data={hourlyValues} labels={hourlyLabels} name={"TODAY"} />
+      <LineGraph data={datavalue} labels={filtereddatakey} name={"TODAY"} />
       <LineGraph data={weeklyData} labels={weeklyLabels} name={"THIS WEEK"} />
 
       <div className="flex flex-col items-center justify-center">
         <section className="flex flex-row gap-[10rem] mt-3 ">
           
-          <Display type={1} name={"Highest"} meas={HighestSpeed} unit={"kph"} />
-          <Display type={1} name={"Lowest"} meas={LowestSpeed} unit={"kph"} />
-          <Display type={1} name={"Average"} meas={AverageSpeed} unit={"kph"} />
+          <Display type={1} name={"Highest"} meas={highestValue} unit={"kph"} />
+          <Display type={1} name={"Lowest"} meas={lowestValue} unit={"kph"} />
+          <Display type={1} name={"Average"} meas={averageValue} unit={"kph"} />
         </section>
       </div>
 

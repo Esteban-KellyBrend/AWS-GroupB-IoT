399bc996c1f1323f3f901549f20f8d5cdd05bb1d
CustodioJustin <Justin.chyle.custodio@adamson.edu.ph>
2023-12-06 16:18:58 +0800

    implement all readltime to other statistics

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,18 +5,23 @@ import Heading from "../components/Heading";
 import Navbar from "../components/Navbar";
 import Temperature from "../components/DashboardComponent/Temperature";
 import Time from "../components/DashboardComponent/Time";
-import Trail from "../components/Trail";
+//import Trail from "../components/Trail";  BROKEN AF
 import { useMostRecentDataFromFirebase } from "../components/database/DataDisplayHandler";
 
 function Dashboard() {
 
   const windspeed_data = useMostRecentDataFromFirebase("Windspeed");
+  const humidity_data = useMostRecentDataFromFirebase("Humidity");
+  const COValue_data = useMostRecentDataFromFirebase("COValue");
+  const Precipitation_data = useMostRecentDataFromFirebase("Precipitation");
+  const HeatIndex_data = useMostRecentDataFromFirebase("HeatIndex");
+  const Temperature_data = useMostRecentDataFromFirebase("Temperature");
+  const SolarIrradiance_data = useMostRecentDataFromFirebase("SolarIrradiance");
 
   return (
     <div className=" bg-gradient-to-tr to-[#431857] from-black from-30% bg-cover absolute h-screen w-screen">
       <Navbar />
       <section className="ml-[10vw] mr-[4vw] z-10 my-3">
-        <Trail />
         <Heading type={1} name={"MAIN DASHBOARD"} />
 
         <Forecast />
@@ -27,32 +32,32 @@ function Dashboard() {
             <Display
               type={1}
               name={"Solar Irradiance"}
-              meas={34.8}
+              meas={SolarIrradiance_data}
               unit={"W/m²"}
             />
             </Link>
               <Link to={"/PrecipitationStats"}>
-            <Display type={1} name={"Precipitation"} meas={"96"} unit={"%"} />
+            <Display type={1} name={"Precipitation"} meas={Precipitation_data} unit={"%"} />
             </Link>
           </section>
 
           <section className="flex flex-col gap-2">
             <Link to={"/TemperatureStats"}>
-          <Temperature meas={69.2} />
+          <Temperature meas={Temperature_data} />
           </Link>
 
             <section className="flex flex-row gap-[5rem]">
             <Link to={"/HumidityStats"}>
-              <Display type={1} name={"Humidity"} meas={42.6} unit={"%"} />
+              <Display type={1} name={"Humidity"} meas={humidity_data} unit={"%"} />
               </Link>
               <Link to={"/HeatIndexStats"}>
-              <Display type={1} name={"Heat Index"} meas={37.2} unit={"°C"} />
+              <Display type={1} name={"Heat Index"} meas={HeatIndex_data} unit={"°C"} />
               </Link>
               
             </section>
             <section className="flex flex-row gap-[5rem]">
             <Link to={"/COValueStats"}>
-              <Display type={1} name={"CO Value"} meas={210} unit={"ppm"} />
+              <Display type={1} name={"CO Value"} meas={COValue_data} unit={"ppm"} />
               </Link>
 
               <Link to={"/WindSpeedStats"}>
--- src/pages/statistics/COValueStats.jsx
+++ src/pages/statistics/COValueStats.jsx
@@ -1,20 +1,42 @@
+import LineGraph from "../../components/LineGraph";
 import Navbar from "../../components/Navbar";
 import Heading from "../../components/Heading";
-import LineGraph from "../../components/LineGraph";
-import Trail from "../../components/Trail";
+import Display from "../../components/Display";
+import { formatDataKeys, GetLowHighAveData, useWeeklyDataFromFirebase, useTodayDataFromFirebase } from "../../components/database/DataDisplayHandler";
 
 function COValueStats() {
+
+  const datahr = useTodayDataFromFirebase("COValue");
+  const datakey = datahr.map(entry => entry.key);
+  const datavalue  = datahr.map(entry => entry.value);
+  const filtereddatakey = formatDataKeys(datakey, "HHMM", "12hour");
+
+  const datawk = useWeeklyDataFromFirebase("COValue");
+  const weeklyLabels = datawk.map(entry => entry.key);
+  const weeklyData = datawk.map(entry => entry.value);
+
+  const [averageValue, lowestValue, highestValue] = GetLowHighAveData(datavalue)
+
   return (
     <div className="bg-gradient-to-tr to-[#431857] from-black from-30% bg-cover absolute h-screen w-screen">
       <Navbar />
-
       <section className="ml-[10vw] mr-[8vw] z-10 my-3">
-        <Trail />
-        <Heading type={1} name={"CO Value"} />
+        <Heading type={1} name={"WIND SPEED"} />
       </section>
-    </div>
-    
 
+      <LineGraph data={datavalue} labels={filtereddatakey} name={"TODAY"} />
+      <LineGraph data={weeklyData} labels={weeklyLabels} name={"THIS WEEK"} />
+
+      <div className="flex flex-col items-center justify-center">
+        <section className="flex flex-row gap-[10rem] mt-3 ">
+          
+          <Display type={1} name={"Highest"} meas={highestValue} unit={"kph"} />
+          <Display type={1} name={"Lowest"} meas={lowestValue} unit={"kph"} />
+          <Display type={1} name={"Average"} meas={averageValue} unit={"kph"} />
+        </section>
+      </div>
+
+    </div>
   );
 }
 
